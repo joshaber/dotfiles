1927a9e891e70df8c9cf6abd5978380b6aac2a20
Allison McMillan <asheren@gmail.com>
2020-05-04 12:51:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 set -x
 git config --global push.default current
 
+export PS1="\[\e[32m\]codespaces\[\e[m\]:\[\e[35m\]*live demos are awesome*:\[\e[34m\]\w\[\e[m\]\\$ "
+
 cd ~/
 
 echo "" >> ~/.bashrc
