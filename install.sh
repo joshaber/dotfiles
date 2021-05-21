e5ee64744a0bbef2071bcbe03fc6e91d5fe27612
Josh Abernathy <josh@github.com>
2021-05-21 16:34:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@ set -x
 git config --global core.editor "vim"
 touch ~/gotmydots.txt
 
+SCRIPTSETUP="$0"
+
+echo $SCRIPTSETUP
+DOTFILESDIRREL=$(dirname $SCRIPTSETUP)
+cd $DOTFILESDIRREL/..
+DOTFILESDIR=$(pwd -P)
+
 source .bash_profile
