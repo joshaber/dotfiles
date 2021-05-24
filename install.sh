a50f48271e31912fed7f5d103c1f88b85b0cd305
Josh Abernathy <josh@github.com>
2021-05-24 09:48:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,9 @@ set -x
 git config --global core.editor "vim"
 touch ~/gotmydots.txt
 
-OG_DIR=$(pwd -P)
-
-SCRIPTSETUP="$0"
-
-echo $SCRIPTSETUP
-DOTFILESDIRREL=$(dirname $SCRIPTSETUP)
-cd $DOTFILESDIRREL
-DOTFILESDIR=$(pwd -P)
-
-source $DOTFILESDIR/.bash_profile
+source .bash_profile
 
 if [[ $(git config --get remote.origin.url) = "https://github.com/github/github" ]]; then
   export IS_GH_GH=true
+  /workspaces/github/bin/setup-codespaces
 fi
