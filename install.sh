83ffc71730a8b8bae1cd52ae6ad6f775d99c654a
Josh Abernathy <josh@github.com>
2021-10-11 15:00:15 +0000

    Do the linking before the expensive work

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ touch ~/gotmydots.txt
 
 source .bash_profile
 
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bin /home/codespace/.local/bin
+
 if [[ -d "/workspaces/github" ]]; then
   export IS_GH_GH=true
   cd "/workspaces/github"
   bin/setup-codespaces
 fi
-
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bin /home/codespace/.local/bin
